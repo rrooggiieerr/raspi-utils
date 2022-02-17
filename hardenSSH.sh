39c8cc561fd5927d74ad4e0322795cd7786a47e3
rrooggiieerr <rogier@batoid.com>
2022-02-17 13:09:52 +0100

    Fix sudo user home dir

--- hardenSSH.sh
+++ hardenSSH.sh
@@ -11,11 +11,13 @@ backupFile() {
 	[ -f "$1" ] && [ ! -f "$1".old$TIMESTAMP ] && cp -p "$1" "$1".old$TIMESTAMP
 }
 
-[ -z "SUDO_USER" ] && SUDO_USER=pi
-[ -z "SUDO_UID" ] && SUDO_UID=1000
-[ -z "SUDO_GID" ] && SUDO_GID=1000
+[ -z "$SUDO_USER" ] && SUDO_USER=pi
+[ -z "$SUDO_UID" ] && SUDO_UID=1000
+[ -z "$SUDO_GID" ] && SUDO_GID=1000
 
-if [ ! -s ~$SUDO_USER/.ssh/authorized_keys ]; then
+SUDO_USER_HOME=`eval echo ~$SUDO_USER`
+
+if [ ! -s $SUDO_USER_HOME/.ssh/authorized_keys ]; then
 	# Ask for SSH public key
 	echo 'It is recommended to use key based authentication for SSH'
 	echo 'Your SSH public key, leave empty to keep using a password instead'
@@ -25,9 +27,10 @@ if [ ! -s ~$SUDO_USER/.ssh/authorized_keys ]; then
 
 	if [ -n "$SSHPUBLICKEY" ]; then
 		echo
-		mkdir -p ~$SUDO_USER/.ssh
-		echo "$SSHPUBLICKEY" >> ~$SUDO_USER/.ssh/authorized_keys
-		chown -R $SUDO_UID:$SUDO_GID ~$SUDO_USER/.ssh
+		mkdir -p $SUDO_USER_HOME/.ssh
+		echo "$SSHPUBLICKEY" >> $SUDO_USER_HOME/.ssh/authorized_keys
+		chown -R $SUDO_UID:$SUDO_GID $SUDO_USER_HOME/.ssh
+		// Disable password of sudo user
 		usermod -p "*" $SUDO_USER
 	elif [ -e /run/sshwarn ]; then
 		# Change user password
@@ -38,7 +41,7 @@ if [ ! -s ~$SUDO_USER/.ssh/authorized_keys ]; then
 fi
 
 # Disable password authentication in SSH daemon if authorized_keys are configured
-if [ -s ~$SUDO_USER/.ssh/authorized_keys ]; then
+if [ -s $SUDO_USER_HOME/.ssh/authorized_keys ]; then
 	echo
 	echo 'You are using key based authentication for SSH, disabling password based authentication'
 	backupFile /etc/ssh/sshd_config
